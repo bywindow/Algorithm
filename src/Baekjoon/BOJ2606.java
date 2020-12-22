70796b9de1de8fbb3a9a43b48e3f28ff80a90e71
dionidip <paulus0617@gmail.com>
2020-12-23 01:12:02 +0900

    2020-12-23

---
+++ src/Baekjoon/BOJ2606.java
@@ -0,0 +1,47 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2606 {
+
+    static int v;
+    static int answer = 0;
+    static int[][] edge;
+    static int[] visited;
+
+    static void bfs(int start){
+        //큐의 길이가 가장 길 때에서 -1을 하면 바이러스가 전파된 컴퓨터의 개수가 된다
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = 1;
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            for(int i = 1; i < v+1; i++){
+                if(visited[i]==0 && edge[cur][i]==1){
+                    q.offer(i);
+                    visited[i] = 1;
+                    answer+=1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        v = Integer.parseInt(br.readLine());//컴퓨터 개수
+        int e = Integer.parseInt(br.readLine());//간선 개수
+        visited = new int[v+1];//방문여부 기록
+        //간선이 연결된 정보
+        edge = new int[v+1][v+1];
+        for(int i = 0; i < e; i++){
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input, " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            edge[x][y] = edge[y][x] = 1;
+        }
+        bfs(1);
+        System.out.println(answer);
+    }
+}
