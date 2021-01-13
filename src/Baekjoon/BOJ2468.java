71eda13e9916269f156789d0c0c3150b00953953
dionidip <paulus0617@gmail.com>
2021-01-13 22:24:42 +0900

    2021-01-13

---
+++ src/Baekjoon/BOJ2468.java
@@ -0,0 +1,58 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2468 {
+
+    static int[][] graph;//높이 정보가 저장된 배열
+    static int highest = 0;//가장 높은 높이
+    static int lowest = 100;//가장 낮은 높이
+    static boolean[][] visited;
+    static int cnt = 0;
+
+    static void dfs(int curH, int r, int c){
+        if(visited[r][c]) return;
+
+        else if(graph[r][c] > curH){
+            visited[r][c] = true;
+            if(r+1 < graph.length) dfs(curH, r+1,  c); //move down
+            if(c+1 < graph[r].length) dfs(curH, r, c+1); //move right
+            if(r-1 >= 0) dfs(curH, r-1, c); //move up
+            if(c-1 >= 0) dfs(curH, r, c-1); //move left
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());// 배열의 크기 n X n
+        int answer= 0;
+        graph = new int[n][n];
+
+        StringTokenizer st;
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                highest = Math.max(highest, graph[i][j]);
+                lowest = Math.min(lowest, graph[i][j]);
+            }
+        }
+        int curH = lowest;
+        while(curH <= highest) {
+            visited = new boolean[n][n];
+            for (int i = 0; i < graph.length; i++) {
+                for (int j = 0; j < graph[i].length; j++) {
+                    if (!visited[i][j] && graph[i][j] > curH) {
+                        dfs(curH, i, j);
+                        cnt++; //dfs를 돌고 나오면 하나의 영역이 생긴 것이다
+                    }
+                }
+            }
+            answer = Math.max(answer, cnt);
+            curH++;
+            cnt = 0;
+        }
+        System.out.println(answer);
+    }
+}
--- src/Bootcamp/Test.java
+++ src/Bootcamp/Test.java
@@ -1,10 +1,37 @@
 package Bootcamp;
 
+import java.util.Scanner;
+
 public class Test {
 
       public static void main(String[] args) {
 
-            int m = 1 + 10 % 3;
-            System.out.println(m);
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            seasonCheck(a);
+            int auth = 0;
+
+            
       }
+
+      static void seasonCheck(int month){
+
+            switch(month){
+                  case 3:
+                  case 4:
+                  case 5:
+                        System.out.println("Spring!");
+                        break;
+                  case 6: case 7: case 8:
+                        System.out.println("Summer!");
+                        break;
+                  case 9: case 10: case 11:
+                        System.out.println("Fall!");
+                        break;
+                  default:
+                        System.out.println("Winter!");
+            }
+      }
+
+
 }
