a49f516be8feb158ef9a656935cb30e8ef8a072a
dionidip <paulus0617@gmail.com>
2021-08-23 22:06:24 +0900

    solve bronze level

---
+++ src/Baekjoon/BOJ11720.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ11720 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            int cur = Integer.parseInt(input.substring(i,i+1));
+            answer += cur;
+        }
+        System.out.println(answer);
+    }
+}
