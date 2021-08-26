ae25f6bf78863e82a71126042192974f4d378450
dionidip <paulus0617@gmail.com>
2021-08-26 23:42:19 +0900

    solve: math

---
+++ src/Baekjoon/BOJ2292.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ2292 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        n -= 1;
+        int answer = 1;
+        while(n > 0){
+            n -= (answer * 6);
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
