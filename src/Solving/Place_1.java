7b50ea14d44517337628d4bb95f7b20eace421f3
dionidip <paulus0617@gmail.com>
2022-04-11 22:06:46 +0900

    solving

---
+++ src/Solving/Place_1.java
@@ -0,0 +1,12 @@
+package Solving;
+
+public class Place_1 {
+
+  public static void main(String[] args) {
+    String sp = "{aaaa}";
+    System.out.println(sp.substring(1, sp.length() - 1));
+    sp = "{bbbbb}";
+    System.out.println(sp);
+  }
+
+}
