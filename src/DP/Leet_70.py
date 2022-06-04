7047052f532d7820c541039b65623e96d3d0e196
dionidip <paulus0617@gmail.com>
2022-06-04 23:50:19 +0900

    solve: dp

---
+++ src/DP/Leet_70.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0 for i in range(46)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, 46):
+            dp[i] = dp[i-2] + dp[i-1]
+        return dp[n]
